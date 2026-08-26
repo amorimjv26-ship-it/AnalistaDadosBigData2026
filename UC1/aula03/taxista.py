26e2310d71e8af9dfa20fba5a43bb7703082ded5
JOAO VICTOR AMORIM ARAUJO <joao.aaraujo@aluno.senac.rio>
2026-08-26 20:37:49 -0300

    exercícios taxista

---
+++ UC1/aula03/taxista.py
@@ -0,0 +1,26 @@
+# Entrada de dados
+
+Km_inicial = 2500
+Km_final = 1700
+Litros = 30
+Valor_recebido = 700
+Distancia = Km_inicial - Km_final
+
+Distancia = 800
+
+consumo_medio = 800 / 30 
+consumo = 26.6 
+gasto_combustivel = Litros * 6.15
+
+lucro = Valor_recebido - gasto_combustivel
+
+print("Resultados")
+print(f"Distância percorrida: {Distancia:.2f} km")
+print(f"Consumo médio: {consumo_medio:.2f} km/L")
+print(f"Gasto com combustível: R$ {gasto_combustivel:.2f}")
+print(f"Lucro líquido: R$ {lucro:.2f}")
+
+
+
+
+
