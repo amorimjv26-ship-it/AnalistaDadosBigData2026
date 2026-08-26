55b60ab47487ea0371e5f2f322d11db29addeb95
JOAO VICTOR AMORIM ARAUJO <joao.aaraujo@aluno.senac.rio>
2026-08-26 19:45:37 -0300

    exercícios azulejos

---
+++ UC1/aula03/azulejos.py
@@ -0,0 +1,21 @@
+# Entrada de dados
+
+comprimento_cozinha = 10
+altura_cozinha = 7
+largura_cozinha = 4
+caixa_azulejos = 1.5
+
+
+#Calcule a quantidade de azulejos necessários
+
+area = ( 2 * comprimento_cozinha * altura_cozinha ) + ( 2 * largura_cozinha * altura_cozinha)
+
+caixa_azulejos = area / 1.5
+
+if caixa_azulejos > int(caixa_azulejos):
+    caixa_azulejos 
+
+print("Area de paredes:" , area)
+print("Quantidade de caixas:", caixa_azulejos)
+ 
+
